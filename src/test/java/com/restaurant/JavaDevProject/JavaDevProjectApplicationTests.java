10892c09a6194ae394b8048e2a46bac3095966a4
HazeelRamirez <hazeelhr11@gmail.com>
2023-05-05 20:56:34 -0600

    project

---
+++ src/test/java/com/restaurant/JavaDevProject/JavaDevProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.JavaDevProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
